01b3f7af4d17a9145def5dced8a40a1203af87e4
pyza <pyznarski.przemyslaw@gmail.com>
2022-01-12 07:31:49 +0100

    Moving HomeViewModel initalization to HomeScreen.

--- app/src/main/java/com/pyza/bloomapp/MainActivity.kt
+++ app/src/main/java/com/pyza/bloomapp/MainActivity.kt
@@ -3,28 +3,12 @@ package com.pyza.bloomapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.pyza.bloomapp.ui.theme.BloomAppTheme
 
 class MainActivity : ComponentActivity() {
-
-    private val homeViewModel:HomeViewModel by viewModels {
-        object : ViewModelProvider.Factory{
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-                val repository= InMemoryPlantService()
-
-                @Suppress("UNCHECKED_CAST")
-                return HomeViewModel(
-                    plantRepository = repository
-                ) as T
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +23,7 @@ class MainActivity : ComponentActivity() {
                         LoginScreen(navController)
                     }
                     composable("home"){
-                        HomeScreen(homeViewModel = homeViewModel)
+                        HomeScreen()
                     }
                 }
             }
